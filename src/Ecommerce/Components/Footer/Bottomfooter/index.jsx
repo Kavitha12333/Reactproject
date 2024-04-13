3e1c1db283eba81c6f74057d80527b4cda592616
kavitha <kavithask8861@gmail.com>
2024-04-13 13:08:43 +0530

    add sometext

--- src/Ecommerce/Components/Footer/Bottomfooter/index.jsx
+++ src/Ecommerce/Components/Footer/Bottomfooter/index.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>Index
+      <h3>this is my bottom footer skfjvh aksfdjgh</h3>
+    </div>
   )
 }
+
 
 export default Index
